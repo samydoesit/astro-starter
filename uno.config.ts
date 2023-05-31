3c40f554469b3f394441b7338fdada2d677449c1
samydoesit <sxburk@gmail.com>
2023-05-31 23:41:16 +0200

    fix: fix uno config

--- uno.config.ts
+++ uno.config.ts
@@ -6,7 +6,6 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
-import type { theme as windTheme } from '@unocss/preset-wind'
 import { presetIcons } from '@unocss/preset-icons'
 
 export default defineConfig({
@@ -15,7 +14,6 @@ export default defineConfig({
     presetWind(),
     presetIcons(),
   ],
-  theme: {} as typeof windTheme,
   shortcuts: [
     ['btn', 'inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'],
     // background
